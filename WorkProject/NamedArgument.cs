8fa46405956c83c5265ad23e6f20075cea50a4c6
ICX\Mikalai.Shymanski <Mikalai.Shymanski@itechart-group.com>
2020-12-04 15:32:56 +0300

    add coment

--- WorkProject/NamedArgument.cs
+++ WorkProject/NamedArgument.cs
@@ -6,15 +6,15 @@ namespace WorkProject
 {
     class NamedArgument
     {
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
-            PrintDetail(age: 31, description: "Aqa Engineer", text: "simple text");
-        }
+        //    PrintDetail(age: 31, description: "Aqa Engineer", text: "simple text");
+        //}
 
-        public static void PrintDetail(string text, int age, string description)
-        {
-            Console.WriteLine($"People who work as {description}, and {age} years old, have some description {text}");
-        }
+        //public static void PrintDetail(string text, int age, string description)
+        //{
+        //    Console.WriteLine($"People who work as {description}, and {age} years old, have some description {text}");
+        //}
     }
 }
